b4d18ba1974c97132736e308ac1a982434d8683f
The Primotion Studio <oyedelenewton@gmail.com>
2024-04-22 18:55:32 +0100

    flask

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""
+starts a flask application
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    """
+    returns homepage
+    """
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb_page():
+    """
+    returns hbnb page
+    """
+    return render_template("100-hbnb.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
